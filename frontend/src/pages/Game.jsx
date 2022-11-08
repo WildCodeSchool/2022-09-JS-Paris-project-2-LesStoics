d53b1c1719ae8882842375b4ba6d385158e49341
KevinLmn <ikebilol@gmail.com>
2022-11-08 16:25:06 +0100

    ajout des boutons avec fonction de combat

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -6,24 +6,28 @@ import { Link } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 import Player from "../components/Player";
 import characterContext from "../context/Characters";
+import picklerick from "../assets/picklerick.png";
+import mrMeeseeks from "../assets/mr_meeseeks.png";
+import pistoportal from "../assets/pistoportal.png";
 
 // import song from "../assets/rickandmortysong.mp3";
 
 function Game() {
   const { character, enemy, random, setWinner, nickname } =
     useContext(characterContext);
+  // eslint-disable-next-line no-unused-vars
   const [life, setLife] = useState(100);
   const [enemyLife, setEnemyLife] = useState(100);
   const [history, setHistory] = useState([]);
 
-  // useEffect(() => {
-  //   if (enemyLife <= 0) {
-  //     setWinner(character);
-  //   }
-  //   if (life <= 0) {
-  //     setWinner(enemy);
-  //   }
-  // }, enemyLife);
+  useEffect(() => {
+    if (enemyLife <= 0) {
+      setWinner(character);
+    }
+    if (life <= 0) {
+      setWinner(enemy);
+    }
+  }, enemyLife);
 
   const mathRandom = (max, min) => {
     const A = Math.floor(Math.random() * (max - min) + 5);
@@ -31,10 +35,6 @@ function Game() {
       return A;
     }
     return 0;
-  };
-
-  const getRandom = (max, min) => {
-    return Math.floor(Math.random() * max + min);
   };
 
   const attackPlayer = mathRandom(55, 20);
@@ -63,94 +63,28 @@ function Game() {
     return lifePlayer;
   };
 
-  const fight = () => {
-    console.log(`OpponentAttack is : ${opponent.attack}`);
-    console.log(`OpponentDefense is : ${opponent.defense}`);
-    console.log(`PlayerAttack is : ${player1.attack}`);
-    console.log(`PlayerDefense is : ${player1.defense}`);
-    while (player1.life > 0 && opponent.life > 0) {
-      let spell = parseInt(prompt("Choisir sort 1 2 ou 3"), 10);
-      console.log(player1.life);
-      console.log(opponent.life);
-      let attackAttempt = Math.random();
-      if (spell === 1) {
-        attackAttempt = Math.random();
-        console.log(attackAttempt);
-        if (attackAttempt) {
-          opponent.life = attack(
-            opponent.life,
-            player1.attack * 1.5,
-            opponent.defense
-          );
-          setEnemyLife(1);
-        } else console.log("Failed!");
-      } else if (spell === 2) {
-        attackAttempt = Math.random();
-        if (attackAttempt < 0.5) {
-          console.log(attackAttempt);
-          opponent.life = attack(
-            opponent.life,
-            player1.attack * 2,
-            opponent.defense
-          );
-          setEnemyLife(2);
-        } else console.log("Failed!", attackAttempt);
-      } else if (spell === 3) {
-        attackAttempt = Math.random();
-        if (attackAttempt < 0.33) {
-          console.log(attackAttempt);
-          opponent.life = attack(
-            opponent.life,
-            player1.attack * 3,
-            opponent.defense
-          );
-          setEnemyLife(3);
-        } else console.log("Failed!", attackAttempt);
-      } else {
-        spell = parseInt(prompt("Choisir sort 1 2 ou 3 svp"), 10);
-      }
-      player1.life = attack(player1.life, opponent.attack, player1.defense);
-      setEnemyLife(opponent.life);
-      console.log(`Player HP is : ${player1.life}`);
-      console.log(`Opponent HP is : ${opponent.life}`);
-      console.log(`Opponent HP state is : ${enemyLife}`);
-    }
-    if (player1.life <= 0) {
-      setWinner(enemy);
-      console.log(`${opponent.name} Won !`);
-    } else {
-      setWinner(character);
-      console.log(`${player1.name} Won !`);
-    }
-  };
+  let damageDone = 0;
+  let newLife = 0;
 
-  const handleOne = () => {
-    opponent.life = attack(enemyLife, player1.attack, opponent.defense);
-    setEnemyLife(opponent.life);
+  const fight = (multiplier, chance) => {
+    const attackAttempt = Math.random();
+    console.log(attackAttempt);
+    if (attackAttempt < chance) {
+      newLife = attack(
+        enemyLife,
+        player1.attack * multiplier,
+        opponent.defense
+      );
+      damageDone = enemyLife - newLife;
 
-    setHistory((y) => [...y, "salut"]);
+      setEnemyLife(newLife);
+      setHistory((y) => [
+        ...y,
+        `${nickname} did ${damageDone} damage to Opponent`,
+      ]);
+    } else setHistory((y) => [...y, `Echec Critique!`]);
     if (enemyLife <= 0) {
       setWinner(character);
-    }
-  };
-
-  const handleTwo = () => {
-    let attackAttempt = Math.random();
-    attackAttempt = Math.random();
-    if (attackAttempt < 0.5) {
-      console.log(attackAttempt);
-      opponent.life = attack(enemyLife, player1.attack * 2, opponent.defense);
-      setEnemyLife(opponent.life);
-    }
-  };
-
-  const handleThree = () => {
-    let attackAttempt = Math.random();
-    attackAttempt = Math.random();
-    if (attackAttempt < 0.33) {
-      console.log(attackAttempt);
-      opponent.life = attack(enemyLife, player1.attack * 3, opponent.defense);
-      setEnemyLife(opponent.life);
     }
   };
 
@@ -163,7 +97,7 @@ function Game() {
             name={character.name}
             image={character.image}
             heart={life}
-            power={getRandom(90, 10)}
+            power={attackPlayer}
           />
           <div className="flex justify-center flex-col items-center gap-5">
             <div className="flex justify-center items-center mx-10">
@@ -173,33 +107,28 @@ function Game() {
                 onClick={random}
                 aria-label="random"
               />
-              <Link to="/winner">
-                <button
-                  type="button"
-                  className="fight"
-                  onClick={fight}
-                  aria-label="fight"
-                />
-              </Link>
+              <Link to="/winner" />
             </div>
             <div>
               <Link to={`${enemyLife <= 0 ? "/winner" : ""}`}>
-                <button type="button" onClick={handleOne}>
-                  ATTACK
-                </button>
-                <button type="button" onClick={handleTwo}>
-                  ATTACK2222
-                </button>
-                <button type="button" onClick={handleThree}>
-                  ATTACK33333
-                </button>
+                <img
+                  src={mrMeeseeks}
+                  alt="Attack One"
+                  onClick={() => fight(1, 1)}
+                />
+                <img
+                  src={pistoportal}
+                  alt="Attack Two"
+                  onClick={() => fight(1.3, 0.8)}
+                />
+                <img
+                  src={picklerick}
+                  alt="Attack Three"
+                  onClick={() => fight(1.6, 0.6)}
+                />
               </Link>
-              <div className="list">
-                <ul>
-                  {history.map((x) => (
-                    <li>{x}</li>
-                  ))}
-                </ul>
+              <div className="list h-4  flex flex-reverse">
+                <ul>{history.map((x) => <li>{x}</li>).reverse()}</ul>
               </div>
             </div>
           </div>
@@ -208,7 +137,7 @@ function Game() {
             name={enemy.name}
             image={enemy.image}
             heart={enemyLife}
-            power={getRandom(90, 10)}
+            power={attackOpponent}
           />
         </>
       ) : (
